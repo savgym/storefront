22d4b534b576691c528273a1de0fd3211a77ad47
Savvas Gymnopoulos <sav_tph@hotmail.com>
2026-03-14 05:39:50 +0200

    my second commit

--- playground/views.py
+++ playground/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.db.models import Q, F
+from store.models import Product
 
 
 def say_hello(request):
-    return render(request, 'hello.html', {'name': 'Savvas'})
+    queryset = Product.objects.filter(inventory=F('unit_price'))
+
+    return render(request, 'hello.html', {'name': 'Savvas', 'products': list(queryset)})
